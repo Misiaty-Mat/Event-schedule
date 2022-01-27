dbfde79dc9db8194f1c5c154777cdc717d45d811
Misiaty-Mat <m.a.s.m@interia.pl>
2022-01-27 21:22:04 +0100

    Added event model with relation to user

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,24 +5,29 @@ from django.utils.translation import gettext as _
 from core import models
 
 
-# class UserAdmin(BaseUserAdmin):
-#     ordering = ['id']
-#     list_display = ['email', 'username']
-#     fieldsets = (
-#         (None, {'fields': ('email', 'password')}),
-#         (_('Personal Info'), {'fields': ('username',)}),
-#         (
-#             _('Permissions'),
-#             {'fields': ('is_active', 'is_staff', 'is_superuser')}
-#         ),
-#         (_('Important dates'), {'fields': ('last_login',)}),
-#     )
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('email', 'password1', 'password2')
-#         }),
-#     )
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['id', 'email', 'username']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('username',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')
+        }),
+    )
 
 
-admin.site.register(models.User)
+class EventAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', )
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Event, EventAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
-#from django.conf import settings
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -34,4 +34,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = 'username'
-        
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    event_time = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
