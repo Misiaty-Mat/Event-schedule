6a6292dbaf441f68fd71ed15d6af41f29cda973b
Misiaty-Mat <m.a.s.m@interia.pl>
2022-01-27 11:50:11 +0100

    Created user register and signup funcional forms

---
+++ app/user_interface/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.sign_up_user, name='signup'),
+    path('login/', views.loginuser, name='login'),
+]
--- app/user_interface/views.py
+++ app/user_interface/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.forms import forms
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from . import forms
+
+from core.models import User
+
+
+def home(request):
+    return render(request, 'ui/index.html')
+
+def sign_up_user(request):
+    if request.method == 'GET':
+        return render(request, 'ui/signUpUser.html', {'form': forms.RegisterForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1'],
+                    email=request.POST['email']
+                )
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(
+                    request, 'ui/signUpUser.html',
+                    {'form': forms.RegisterForm(), 'error': 'Username or email is already taken.'}
+                )
+        else:
+            return render(
+                request, 'ui/signUpUser.html',
+                {'form': forms.RegisterForm(), 'error': 'Passwords did not match.'}
+            )
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'ui/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'ui/login.html', {'form': AuthenticationForm(), 'error': "Username or password are incorrect"})
+        else:
+            login(request, user)
+            return redirect('home')
+
+@login_required
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
